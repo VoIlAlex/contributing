4652ffed4ceb597b9738d7a3050abeae4a4d3dec
VoIlAlex <ilya.vouk@gmail.com>
2020-04-15 12:32:34 +0300

    Separate main function from arguments parsing

--- contributing/cli.py
+++ contributing/cli.py
@@ -2,12 +2,15 @@ import click
 from .fetch import get_contributing_md
 
 
-@click.command()
-@click.option('-o', '--output', required=False, default='CONTRIBUTING.md')
-def cli(output):
+def main(output):
     contributing_md = get_contributing_md()
     with open(output, 'w+') as f:
         f.write(contributing_md)
+
+@click.command()
+@click.option('-o', '--output', required=False, default='CONTRIBUTING.md')
+def cli(*args, **kwargs):
+    main(*args, **kwargs)
 
 
 if __name__ == "__main__":
